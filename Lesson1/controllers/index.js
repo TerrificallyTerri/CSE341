3f06e2dc23baa8d3ac5a281d717da1e7f7880d09
terrificallyterri <114156687+TerrificallyTerri@users.noreply.github.com>
2025-05-11 15:16:10 +0200

    Delete Lesson1 directory
    Not working right

--- Lesson1/controllers/index.js
+++
@@ -1,9 +0,0 @@
-displayData = (req, res) => { // Define a function to handle requests to the root URL
-    const data = // Create an object with some data
-        'This is display data';
-    res.status(200).send(data);
-};
-
-module.exports = { // Export the function so it can be used in other files
-    displayData, // Export the displayData function
-};
